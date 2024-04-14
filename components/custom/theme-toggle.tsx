cc9aeda86a697c4c84ac98b1dd258640c5e907ae
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-14 16:43:59 +0100

    remove hover state

--- components/custom/theme-toggle.tsx
+++ components/custom/theme-toggle.tsx
@@ -12,6 +12,7 @@ export const ThemeToggle = () => {
       variant="ghost"
       size="icon"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      className="hover:dark:bg-transparent hover:bg-transparent"
     >
       <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
       <Moon className="hidden h-5 w-5 dark:block" />
