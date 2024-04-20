2b0fa8d9b18723d747b6d60276b8da91286214ed
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-20 14:51:48 +0100

    array of skills

---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+export const skills = [
+  'Typescript/Javascript',
+  'C',
+  'Next.Js/React',
+  'Node JS',
+  'Express.JS',
+  'Tailwind CSS',
+  'PostgreSQL',
+  'mySQL',
+  'MongoDB',
+  'Git/Github',
+  'Neovim/Vim',
+];
