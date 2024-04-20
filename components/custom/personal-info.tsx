21bdcc61130f28e9275c5dc9dd742a23f49294ca
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-20 15:30:46 +0100

    Eslint war against apostrophes

--- components/custom/personal-info.tsx
+++ components/custom/personal-info.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 const PersonalInfo = () => {
   return (
     <section className="lg:max-w-[65%] mt-20">
-      <h4 className="text-lg">Hi, I'm Samuel Oluwadabira Adigun.</h4>
+      <h4 className="text-lg">Hi, I&apos;m Samuel Oluwadabira Adigun.</h4>
       <h2 className="mt-3 text-4xl font-bold dark:text-[#E3E3E3]">
-        I'm a software developer based in Ibadan, Nigeria.
+        I&apos;m a software developer based in Ibadan, Nigeria.
       </h2>
       <p className="mt-3">
-        I'm deeply passionate about crafting applications that tackle real-world
-        challenges and contribute positively to society. At the moment, I'm
-        actively seeking new opportunities while unwinding with my favorite
-        pastimes: listening to music and reading different forms of literature.
+        I&apos;m deeply passionate about crafting applications that tackle
+        real-world challenges and contribute positively to society. At the
+        moment, I&apos;m actively seeking new opportunities while unwinding with
+        my favorite pastimes: listening to music and reading different forms of
+        literature.
       </p>
     </section>
   );
