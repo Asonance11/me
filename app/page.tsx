bc6dbb67aa13a1da5ab349b920d603958be3c08c
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-20 14:30:42 +0100

    Experience and Education

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ExperienceAndEducation from '@/components/custom/experience-and-education';
 import PersonalInfo from '@/components/custom/personal-info';
 
 export default function Home() {
   return (
     <main>
       <PersonalInfo />
+      <ExperienceAndEducation />
     </main>
   );
 }
