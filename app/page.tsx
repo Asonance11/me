9d64d0503856b2ce07dd411d236d36e0db304b11
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-27 19:45:45 +0100

    Add tracing beam

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import ExperienceAndEducation from '@/components/custom/experience-and-education';
 import PersonalInfo from '@/components/custom/personal-info';
+import { TracingBeam } from '@/components/ui/tracing-beam';
 
 export default function Home() {
   return (
+    <TracingBeam>
     <main>
       <PersonalInfo />
       <ExperienceAndEducation />
     </main>
+    </TracingBeam>
   );
 }
