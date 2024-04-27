70de0b8df62dc74e96436db7574e4aa18017e8d8
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-27 19:45:30 +0100

    Addtion of plugin to change tailwind colors to variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import { fontFamily } from 'tailwindcss/defaultTheme';
+import colors from "tailwindcss/colors"
 import type { Config } from 'tailwindcss';
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ['class'],
@@ -78,7 +82,21 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'),
+    addVariablesForColors
+  ],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
