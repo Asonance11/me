2e45e8638aea4f653ccebbe2c10903422f11c930
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-29 15:27:57 +0100

    Added a Footer

---
+++ components/custom/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-20 mb-10 text-sm w-full flex flex-col items-center justify-center gap-y-2">
+      <p>Built and Designed by Adigun Samuel</p>
+      <p>All rights reserved &copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
