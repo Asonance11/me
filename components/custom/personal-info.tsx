4ec48f69749e468ac15297d0d324a98de73dc699
Oluwadabira <adigunsamuelaso@gmail.com>
2024-07-12 10:46:27 +0100

    Update

--- components/custom/personal-info.tsx
+++ components/custom/personal-info.tsx
@@ -8,7 +8,7 @@ const PersonalInfo = () => {
         <div className="lg:max-w-[65%] mt-20">
           <h4 className="text-lg">Hi, I&apos;m Samuel Oluwadabira Adigun.</h4>
           <h2 className="mt-4 text-5xl font-bold dark:text-[#E3E3E3]">
-            I&apos;m a software developer based in Lagos, Nigeria.
+            I&apos;m a software developer based in Nigeria.
           </h2>
           <p className="mt-4 text-base">
             I&apos;m deeply passionate about crafting applications that tackle
